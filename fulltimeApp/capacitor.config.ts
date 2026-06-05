703ecef8739cb9aae211265a7db1edec2be245fc
Alex Andrango <98037053+CP-Avances@users.noreply.github.com>
2026-06-05 10:59:49 -0500

    commit para pruebas locales

--- fulltimeApp/capacitor.config.ts
+++ fulltimeApp/capacitor.config.ts
@@ -7,7 +7,7 @@ const config: CapacitorConfig = {
   webDir: 'www',
   server: {
     cleartext: false,
-    androidScheme: 'https',
+    androidScheme: 'http',
   },
   plugins: {
     PushNotifications: {
