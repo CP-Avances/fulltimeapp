74b31501046a49efb9c493e1be09a1d713245ea3
Alex Andrango <98037053+CP-Avances@users.noreply.github.com>
2026-07-08 10:11:49 -0500

    Commit Version 4.0.0 Produccion

--- fulltimeApp/src/environments/environment.prod.ts
+++ fulltimeApp/src/environments/environment.prod.ts
@@ -1,14 +1,19 @@
 export const environment = {
   production: true,
 
-   //DESARROLLO
-  urlMultitenant: 'http://192.168.0.145:3011',
+  //DESARROLLO
+  /*urlMultitenant: 'http://192.168.0.145:3011',
   reportesURL: 'http://localhost:8080/api/reporte', // Microservicio de reportes
-  socketUrl: 'http://192.168.0.145:3011',
+  socketUrl: 'http://192.168.0.145:3011',*/
 
   //PRODUCCION
   /*urlMultitenant: 'https://conexionfulltime.fulltime.com.ec',
   reportesURL: 'https://reportesfulltime.fulltime.com.ec/api/reporte',
   socketUrl: 'https://conexionfulltime.fulltime.com.ec',*/
+
+  //NUBE
+  urlMultitenant: 'https://conexion.aqhora.com',
+  reportesURL: 'https://reportes.aqhora.com/api/reporte',
+  socketUrl: 'https://conexion.aqhora.com'
 
 };
